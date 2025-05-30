eaaed510392d8e8acc81b9aa1240b06ee6ce8bb8
Brendan Brown <brendan94brown@gmail.com>
2025-05-29 21:04:13 -0700

    Add calculations for sum and average of store annual sales; add count query for stores over 1 million annual revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+
+total_revenue = Store.sum(:annual_revenue)
+puts total_revenue
+
+average_revenue = Store.average(:annual_revenue)
+puts average_revenue
+
+puts Store.where("annual_revenue > ?", 1000000).count
