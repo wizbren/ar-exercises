b59628a3277c63049a936a42c2a59718c0e984e6
Brendan Brown <brendan94brown@gmail.com>
2025-05-29 21:19:01 -0700

    Add one-to-many relationships between store and employees; create 4 sample employees for store1 and store 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,13 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Brendan", last_name: "Brown", hourly_rate: 1337)
+@store1.employees.create(first_name: "Ryan", last_name: "Brown", hourly_rate: 1336)
+@store1.employees.create(first_name: "Jeff", last_name: "Stinson", hourly_rate: 0)
+
+@store2.employees.create(first_name: "Captain", last_name: "Ginyu", hourly_rate: 100)
+@store2.employees.create(first_name: "Lord", last_name: "Vegeta", hourly_rate: 250)
+@store2.employees.create(first_name: "Caladan", last_name: "Brood", hourly_rate: 900)
+@store2.employees.create(first_name: "Anomander", last_name: "Rake", hourly_rate: 900)
